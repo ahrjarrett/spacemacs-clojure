8b487c0d6cf16da859eda6476a8b6e8fad6f4792
Andrew Jarrett <ahrjarrett@gmail.com>
2017-05-09 19:17:01 -0600

    requiring with aliases

--- src/spacemacs_clojure/core.clj
+++ src/spacemacs_clojure/core.clj
@@ -1,9 +1,21 @@
+;; namespaces, require, use and import
+
+;; If we were running these things in the REPL,
+;; we would need to require these libs:
+
+;;    (require 'clojure.string)
+;;    (require 'clojure.test)
+
 (ns spacemacs-clojure.core)
-(require 'clojure.string)
+
+;; You can require namespaces with an alias using the keyword :as, like so:
+(require '[clojure.string :as string])
 
 (defn foo
   [x]
-  (clojure.string/split x #","))
+  (string/split x #","))
 
-(foo "sup,dude")
+(foo "sup,dude") ;; ["sup" "dude"]
 
+(clojure.test/is (= 1 2)) ;; false
+(clojure.test/is (= "server.js" "server.js")) ;; true
